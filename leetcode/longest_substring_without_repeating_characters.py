5432e83fac3ef014c629f2cf6058f52a87421880
dantin <chengjie.ding@gmail.com>
2021-03-17 10:55:08 +0800

    add implementation.

--- leetcode/longest_substring_without_repeating_characters.py
+++ leetcode/longest_substring_without_repeating_characters.py
@@ -3,7 +3,23 @@
 
 class Solution:
     def lengthOfLongestSubstring(self, s: str) -> int:
-        return 0
+        cache = {}
+        max_len = 0
+        length = 0
+        i = 0
+        while i < len(s):
+            c = s[i]
+            if c not in cache:
+                cache[c] = i
+                length += 1
+                i += 1
+            else:
+                max_len = max(max_len, length)
+                length = 0
+                i = cache[c] + 1
+                cache = {}
+
+        return max_len
 
 
 if __name__ == '__main__':
---
+++ tests/test_longest_substring_without_repeating_characters.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+import pytest
+
+
+@pytest.fixture
+def solution():
+    from leetcode.longest_substring_without_repeating_characters import Solution
+    return Solution()
+
+
+@pytest.mark.parametrize(
+    's,expected',
+    [
+        ('abcabcbb', 3),
+        ('bbbbb', 1),
+        ('pwwkew', 3),
+    ],
+)
+def test_longest_substring_without_repeating_characters(solution, s, expected):
+    actual = solution.lengthOfLongestSubstring(s)
+    assert expected == actual
