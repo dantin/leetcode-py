60febe4fe8e06bfd19d462cb2fc6da093a0e154e
dantin <chengjie.ding@gmail.com>
2021-03-22 15:36:48 +0800

    add base code structure of reverse integer.

---
+++ leetcode/reverse-integer.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+
+class Solution:
+    def reverse(self, x: int) -> int:
+        pass
+
+
+if __name__ == '__main__':
+    x = 123
+    print(f' Input: x = {x}')
+    s = Solution()
+    y = s.reverse(x)
+    print(f' Output: {y}')
