60124dfb68d6ee00c5927fb1d94f5a93e8c0473f
dantin <chengjie.ding@gmail.com>
2021-03-23 10:47:16 +0800

    add code structure of "palindrome number".

---
+++ leetcode/palindrome_number.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+
+class Solution:
+    def isPalindrome(self, x: int) -> bool:
+        return True
+
+
+if __name__ == '__main__':
+    x = 121
+    print(f' Input: x = {x}')
+    s = Solution()
+    y = s.isPalindrome(x)
+    print(f' Output: {y}')
