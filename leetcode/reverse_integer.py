b6e471d7c329f3caba1933677a531d8d36c834cc
David Ding <chengjie.ding@gmail.com>
2021-03-23 09:09:42 +0800

    Add solution of reverse integer. (#7)
    * add base code structure of reverse integer.
    
    * add implementation.
    
    * add test cases.

---
+++ leetcode/reverse_integer.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+
+class Solution:
+    def reverse(self, x: int) -> int:
+        is_neg = True if x < 0 else False
+        if x < 0:
+            x = -x
+
+        n, limit = 0, 2 ** 31
+        while x > 0:
+            n = n * 10
+            n += x % 10
+            x //= 10
+        if is_neg:
+            return -n if n <= limit else 0
+        else:
+            return n if n < limit else 0
+
+
+if __name__ == '__main__':
+    x = 123
+    print(f' Input: x = {x}')
+    s = Solution()
+    y = s.reverse(x)
+    print(f' Output: {y}')
