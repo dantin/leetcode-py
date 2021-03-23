210263e3b28c5724dd5e04499ec357435a44a976
dantin <chengjie.ding@gmail.com>
2021-03-23 08:55:39 +0800

    add implementation.

--- leetcode/reverse-integer.py
+++ leetcode/reverse-integer.py
@@ -2,7 +2,20 @@
 
 class Solution:
     def reverse(self, x: int) -> int:
-        pass
+        is_neg = True if x < 0 else False
+        if x < 0:
+            x = -x
+
+        limit = 2 ** 31
+        n = 0
+        while x > 0:
+            n = n * 10
+            n += x % 10
+            x //= 10
+        if is_neg:
+            return 0 if n > limit else -n
+        else:
+            return 0 if n >= limit else n
 
 
 if __name__ == '__main__':
