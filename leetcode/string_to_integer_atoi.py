12d891dec7abdfbcf434e3f2c2d667a6267959e7
dantin <chengjie.ding@gmail.com>
2021-03-23 10:29:38 +0800

    add implementation.

--- leetcode/string_to_integer_atoi.py
+++ leetcode/string_to_integer_atoi.py
@@ -2,7 +2,29 @@
 
 class Solution:
     def myAtoi(self, s: str) -> int:
-        return s
+        start, n = 0, 0
+        for c in s:
+            if c == ' ':
+                start += 1
+            else:
+                break
+
+        is_neg = True if s[start] == '-' else False
+        if s[start] in ('+', '-'):
+            start += 1
+
+        for c in s[start:]:
+            if str.isdigit(c):
+                n *= 10
+                n += int(c)
+            else:
+                break
+
+        limit = 2 ** 31
+        if is_neg:
+            return -n if n < limit else -limit
+        else:
+            return n if n < limit else limit - 1
 
 
 if __name__ == '__main__':
