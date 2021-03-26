5b2dc5099eb267b651178319e9152fbd8f379c03
dantin <chengjie.ding@gmail.com>
2021-03-26 11:21:23 +0800

    add implementation.

--- leetcode/container_with_most_water.py
+++ leetcode/container_with_most_water.py
@@ -4,7 +4,16 @@ from typing import List
 
 class Solution:
     def maxArea(self, height: List[int]) -> int:
-        return 0
+        lhs, rhs = 0, len(height) - 1
+        max_area = 0
+        while lhs < rhs:
+            area = min(height[lhs], height[rhs]) * (rhs - lhs)
+            max_area = max(area, max_area)
+            if height[lhs] <= height[rhs]:
+                lhs += 1
+            else:
+                rhs -= 1
+        return max_area
 
 
 if __name__ == '__main__':
