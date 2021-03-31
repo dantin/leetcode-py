128c812b8857014f0ac48e3e2fdce5a74838448d
David Ding <chengjie.ding@gmail.com>
2021-03-31 15:51:44 +0800

    Add solution of "longest common prefix". (#14)
    * Add solution of "longest common prefix".
    
    * add implementation.

---
+++ leetcode/longest_common_prefix.py
@@ -0,0 +1,70 @@
+# -*- coding: utf-8 -*-
+from typing import List
+
+
+class Solution:
+    def longestCommonPrefix(self, strs: List[str]) -> str:
+        if not strs:
+            return ''
+
+        def lcp(s1: str, s2: str) -> str:
+            length, i = min(len(s1), len(s2)), 0
+            while i < length and s1[i] == s2[i]:
+                i += 1
+            return s1[:i]
+
+        prefix, size = strs[0], len(strs)
+        for i in range(1, size):
+            prefix = lcp(prefix, strs[i])
+        return prefix
+
+    def longestCommonPrefix2(self, strs: List[str]) -> str:
+        if not strs:
+            return ''
+        length, size = len(strs[0]), len(strs)
+        for i in range(length):
+            c = strs[0][i]
+            if any(i == len(strs[j]) or strs[j][i] != c for j in range(1, size)):
+                return strs[0][:i]
+        return strs[0]
+
+    def longestCommonPrefix3(self, strs: List[str]) -> str:
+        def lcp(start: int, end: int):
+            if start == end:
+                return strs[start]
+
+            mid = (start + end) // 2
+            left, right = lcp(start, mid), lcp(mid + 1, end)
+            min_len = min(len(left), len(right))
+            for i in range(min_len):
+                if left[i] != right[i]:
+                    return left[:i]
+            return left[:min_len]
+
+        return '' if not strs else lcp(0, len(strs) - 1)
+
+    def longestCommonPrefix4(self, strs: List[str]) -> str:
+        def is_common_prefix(length):
+            s0, size = strs[0][:length], len(strs)
+            return all(strs[i][:length] == s0 for i in range(1, size))
+
+        if not strs:
+            return ''
+
+        min_len = min(len(s) for s in strs)
+        low, high = 0, min_len
+        while low < high:
+            mid = (high - low + 1) // 2 + low
+            if is_common_prefix(mid):
+                low = mid
+            else:
+                high = mid - 1
+        return strs[0][:low]
+
+
+if __name__ == '__main__':
+    strs = ['flower', 'flow', 'flight']
+    print(f' Input: strs = {strs}')
+    s = Solution()
+    r = s.longestCommonPrefix4(strs)
+    print(f' Output: {r}')
---
+++ tests/test_longest_common_prefix.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+import pytest
+
+
+@pytest.fixture
+def solution():
+    from leetcode.longest_common_prefix import Solution
+    return Solution()
+
+
+@pytest.mark.parametrize(
+    'strs,expected',
+    [
+        (['flower', 'flow', 'flight'], 'fl'),
+        (['dog', 'racecar', 'car'], '')
+    ],
+)
+def test_longest_common_prefix(solution, strs, expected):
+    actual = solution.longestCommonPrefix(strs)
+    assert actual == expected
+    actual = solution.longestCommonPrefix2(strs)
+    assert actual == expected
+    actual = solution.longestCommonPrefix3(strs)
+    assert actual == expected
+    actual = solution.longestCommonPrefix4(strs)
+    assert actual == expected
