6ac51a13745b3249568c6c8c983cb14ee030e41d
dantin <chengjie.ding@gmail.com>
2021-04-09 11:19:22 +0800

    add implementation.

--- leetcode/remove_nth_node_from_end_of_list.py
+++ leetcode/remove_nth_node_from_end_of_list.py
@@ -1,4 +1,6 @@
 # -*- coding: utf-8 -*-
+from typing import List
+
 
 class ListNode:
     def __init__(self, val=0, next=None):
@@ -8,8 +10,44 @@ class ListNode:
 
 class Solution:
     def removeNthFromEnd(self, head: ListNode, n: int) -> ListNode:
-        return None
+        dummy = ListNode(0, head)
+        first, second = head, dummy
+
+        for i in range(n):
+            first = first.next
+
+        while first:
+            first = first.next
+            second = second.next
+
+        second.next = second.next.next
+        return dummy.next
+
+
+def build_list(nums: List[int]) -> ListNode:
+    dummy = ListNode()
+    tail = dummy
+    for n in nums:
+        node = ListNode(n)
+        tail.next = node
+        tail = node
+    return dummy.next
+
+
+def encode_list(head: ListNode) -> List[int]:
+    res = []
+    while head is not None:
+        res.append(head.val)
+        head = head.next
+    return res
 
 
 if __name__ == '__main__':
-    pass
+    head = [1, 2, 3, 4, 5]
+    n = 2
+    print(f' Input head = {head}, n = {n}')
+    head = build_list(head)
+    s = Solution()
+    node = s.removeNthFromEnd(head, n)
+    nums = encode_list(node)
+    print(f' Output: {nums}')
--- tests/test_remove_nth_node_from_end_of_list.py
+++ tests/test_remove_nth_node_from_end_of_list.py
@@ -1,1 +1,24 @@
 # -*- coding: utf-8 -*-
+import pytest
+
+
+@pytest.fixture
+def solution():
+    from leetcode.remove_nth_node_from_end_of_list import Solution
+    return Solution()
+
+
+@pytest.mark.parametrize(
+    'head,n,expected',
+    [
+        ([1, 2, 3, 4, 5], 2, [1, 2, 3, 5]),
+        ([1], 1, []),
+        ([1, 2], 1, [1]),
+    ],
+)
+def test_remove_nth_node_from_end_of_list(solution, head, n, expected):
+    from leetcode.remove_nth_node_from_end_of_list import build_list, encode_list
+    orig = build_list(head)
+    node = solution.removeNthFromEnd(orig, n)
+    actual = encode_list(node)
+    assert actual == expected
