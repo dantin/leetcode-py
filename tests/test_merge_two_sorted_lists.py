d438d85b37dd99a65d89f6b26f0398c771377914
dantin <chengjie.ding@gmail.com>
2021-04-12 10:20:06 +0800

    Add solution of "merge two sorted lists".

---
+++ tests/test_merge_two_sorted_lists.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+import pytest
+
+
+@pytest.fixture
+def solution():
+    from leetcode.merge_two_sorted_lists import Solution
+    return Solution()
+
+
+@pytest.mark.parametrize(
+    'nums1,nums2,expected',
+    [
+        ([1, 2, 4], [1, 3, 4], [1, 1, 2, 3, 4, 4]),
+        ([], [], []),
+        ([], [0], [0]),
+    ],
+)
+def test_merge_two_sorted_lists(solution, nums1, nums2, expected):
+    from leetcode.merge_two_sorted_lists import make_list, dump_list
+    l1 = make_list(nums1)
+    l2 = make_list(nums2)
+    x = solution.mergeTwoLists(l1, l2)
+    actual = dump_list(x)
+    assert actual == expected
