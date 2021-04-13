796618dedd0caf355907f9761496ada2c9b0700c
David Ding <chengjie.ding@gmail.com>
2021-04-13 11:52:28 +0800

    add another solution. (#24)

--- leetcode/merge_k_sorted_lists.py
+++ leetcode/merge_k_sorted_lists.py
@@ -27,6 +27,35 @@ class Solution:
                 lists[i] = lists[i].next
         return dummy.next
 
+    def mergeKLists2(self, lists: List[ListNode]) -> ListNode:
+        if not lists:
+            return None
+
+        def merge_list(l1: ListNode, l2: ListNode) -> ListNode:
+            dummy = ListNode()
+            tail = dummy
+            while l1 and l2:
+                if l1.val <= l2.val:
+                    tail.next = l1
+                    l1 = l1.next
+                else:
+                    tail.next = l2
+                    l2 = l2.next
+                tail = tail.next
+
+            tail.next = l1 if l1 else l2
+            return dummy.next
+
+        def merge(lists: List[ListNode], left: int, right: int) -> ListNode:
+            if left == right:
+                return lists[left]
+            mid = left + (right - left) // 2
+            l1 = merge(lists, left, mid)
+            l2 = merge(lists, mid + 1, right)
+            return merge_list(l1, l2)
+        n = len(lists)
+        return merge(lists, 0, n - 1)
+
 
 def make_list(nums: List[int]) -> ListNode:
     dummy = ListNode()
