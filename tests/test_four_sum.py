13ec03288293b1c02be7cfe9b5bddbff4c574d63
dantin <chengjie.ding@gmail.com>
2021-04-23 16:49:41 +0800

    fix typo.

--- tests/test_four_sum.py
+++ tests/test_four_sum.py
@@ -9,7 +9,7 @@ def solution():
 
 
 @pytest.mark.parametrize(
-    'nums,target, expected',
+    'nums,target,expected',
     [
         ([1, 0, -1, 0, -2, 2], 0, [[-2, -1, 1, 2], [-2, 0, 0, 2], [-1, 0, 0, 1]]),
         ([], 0, []),
