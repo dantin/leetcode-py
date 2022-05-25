e125058d28d864b7587c76c4fe13472841d6e95c
dantin <chengjie.ding@gmail.com>
2022-05-25 03:04:11 -0400

    Add solution of "unique substrings in wraparound string".

--- leetcode/longest_valid_parentheses.py
+++ leetcode/longest_valid_parentheses.py
@@ -2,17 +2,20 @@
 
 class Solution:
     def longestValidParentheses(self, s: str) -> int:
-        max_val = 0
+        longest = 0
+        dp = [0] * len(s)
 
-        dp = [0 for _ in s]
         for i in range(1, len(s)):
             if s[i] == ')':
                 if s[i - 1] == '(':
-                    val = dp[i - 2] if i >= 2 else 0
-                    dp[i] = val + 2
+                    # '...()' extends valid parenthes.
+                    pre = dp[i - 2] if i >= 2 else 0
+                    dp[i] = pre + 2
                 elif i - dp[i - 1] > 0 and s[i - dp[i - 1] - 1] == '(':
-                    val = dp[i - dp[i - 1] - 2] if (i - dp[i - 1]) >= 2 else 0
-                    dp[i] = dp[i - 1] + val + 2
-                max_val = max(max_val, dp[i])
+                    # '((...))' extends previous one.
+                    pre = dp[i - dp[i - 1] - 2] if (i - dp[i - 1]) >= 2 else 0
+                    dp[i] = dp[i - 1] + pre + 2
+                # update longest.
+                longest = max(longest, dp[i])
 
-        return max_val
+        return longest
