c9dbf7f4bf51a2fd1d8b1cff0df7f9450fbb6bfb
David Ding <chengjie.ding@gmail.com>
2022-05-29 11:07:58 +0800

    Refactoring code. (#93)

--- leetcode/valid_sudoku.py
+++ leetcode/valid_sudoku.py
@@ -4,18 +4,24 @@ from typing import List
 
 class Solution:
     def isValidSudoku(self, board: List[List[str]]) -> bool:
-        rows = [{} for i in range(9)]
-        columns = [{} for i in range(9)]
-        boxes = [{} for i in range(9)]
+        # notice shadow copy.
+        rows = [[0] * 9 for _ in range(9)]
+        columns = [[0] * 9 for _ in range(9)]
+        boxes = [[0] * 9 for _ in range(9)]
+
         for i in range(9):
             for j in range(9):
-                num = board[i][j]
-                if num != '.':
-                    num = int(num)
-                    k = (i // 3) * 3 + j // 3
-                    rows[i][num] = rows[i].get(num, 0) + 1
-                    columns[j][num] = columns[j].get(num, 0) + 1
-                    boxes[k][num] = boxes[k].get(num, 0) + 1
-                    if rows[i][num] > 1 or columns[j][num] > 1 or boxes[k][num] > 1:
-                        return False
+                ch = board[i][j]
+                if ch == '.':
+                    continue
+
+                n = int(ch) - 1
+                k = (i // 3) * 3 + j // 3
+
+                rows[i][n] += 1
+                columns[j][n] += 1
+                boxes[k][n] += 1
+
+                if rows[i][n] > 1 or columns[j][n] > 1 or boxes[k][n] > 1:
+                    return False
         return True
