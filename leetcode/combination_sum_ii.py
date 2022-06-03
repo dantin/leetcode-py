c97da96d6d4cb28b8e0cfaa85e1f6d0f4406913c
dantin <chengjie.ding@gmail.com>
2022-06-02 21:56:28 -0400

    Refactoring code.

--- leetcode/combination_sum_ii.py
+++ leetcode/combination_sum_ii.py
@@ -5,24 +5,25 @@ from typing import List
 
 class Solution:
     def combinationSum2(self, candidates: List[int], target: int) -> List[List[int]]:
-        def dfs(pos: int, rest: int) -> None:
-            nonlocal path
-            if rest == 0:
-                res.append(path[:])
+        def dfs(pos: int, total: int) -> None:
+            nonlocal nums
+            if total == 0:
+                combinations.append(nums[:])
                 return
-            if pos == len(freq) or rest < freq[pos][0]:
+            if pos == len(freq) or total < freq[pos][0]:
                 return
 
-            dfs(pos + 1, rest)
+            dfs(pos + 1, total)
 
-            most = min(rest // freq[pos][0], freq[pos][1])
-            for i in range(1, most + 1):
-                path.append(freq[pos][0])
-                dfs(pos + 1, rest - i * freq[pos][0])
-            path = path[:-most]
+            count = min(total // freq[pos][0], freq[pos][1])
+            for i in range(1, count + 1):
+                nums.append(freq[pos][0])
+                dfs(pos + 1, total - i * freq[pos][0])
+            nums = nums[:-count]
 
+        # freq is the sorted mapping of `(num, freq)`
         freq = sorted(collections.Counter(candidates).items())
-        res = []
-        path = []
+        combinations = []
+        nums = []
         dfs(0, target)
-        return res
+        return combinations
