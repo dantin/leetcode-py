bf95923e0885ad2d3eec85d600bd67f08cd7d247
dantin <chengjie.ding@gmail.com>
2023-09-25 13:02:04 +0800

    update code.

--- leetcode/median_of_two_sorted_arrays.py
+++ leetcode/median_of_two_sorted_arrays.py
@@ -5,34 +5,41 @@ from typing import List
 class Solution:
     def findMedianSortedArrays(self, nums1: List[int], nums2: List[int]) -> float:
         len1, len2 = len(nums1), len(nums2)
-        mid = (len1 + len2) // 2
         if (len1 + len2) % 2 == 1:
-            return float(find_kth(nums1, nums2, mid + 1))
+            return float(find_kth(nums1, 0, len1, nums2, 0, len2, (len1 + len2 + 1) // 2))
         else:
-            lhs = find_kth(nums1, nums2, mid)
-            rhs = find_kth(nums1, nums2, mid + 1)
+            mid = (len1 + len2) // 2
+            lhs = find_kth(nums1, 0, len(nums1), nums2, 0, len(nums2), mid)
+            rhs = find_kth(nums1, 0, len(nums1), nums2, 0, len(nums2), mid + 1)
             return (lhs + rhs) / 2.0
 
 
-def find_kth(nums1: List[int], nums2: List[int], k: int) -> int:
+def find_kth(
+    nums1: List[int], begin1: int, end1: int,
+    nums2: List[int], begin2: int, end2: int,
+    k: int,
+) -> int:
     """find kth element in num1 and num2, k is index start from 1."""
-    len1, len2 = len(nums1), len(nums2)
-    # make sure that nums1 is always the smaller one.
-    if len1 > len2:
-        return find_kth(nums2, nums1, k)
+    n = end1 - begin1
+    m = end2 - begin2
 
-    if len1 == 0:  # num1 is empty
-        return nums2[k - 1]
-
+    if n <= 0:
+        return nums2[begin2 + k - 1]
+    if m <= 0:
+        return nums1[begin1 + k - 1]
     if k == 1:
-        return min(nums1[0], nums2[0])
+        return min(nums1[begin1], nums2[begin2])
 
     # devide k into two parts.
-    i1 = min(k // 2, len1)
-    i2 = k - i1
-    if nums1[i1 - 1] < nums2[i2 - 1]:
-        return find_kth(nums1[i1:], nums2[:], k - i1)
-    elif nums1[i1 - 1] > nums2[i2 - 1]:
-        return find_kth(nums1[:], nums2[i2:], k - i2)
+    mid1 = (begin1 + end1) // 2
+    mid2 = (begin2 + end2) // 2
+    if nums1[mid1] <= nums2[mid2]:
+        if (n // 2 + m // 2 + 1) >= k:
+            return find_kth(nums1, begin1, end1, nums2, begin2, mid2, k)
+        else:
+            return find_kth(nums1, mid1 + 1, end1, nums2, begin2, end2, k - n // 2 - 1)
     else:
-        return nums1[i1 - 1]
+        if (n // 2 + m // 2 + 1) >= k:
+            return find_kth(nums1, begin1, mid1, nums2, begin2, end2, k)
+        else:
+            return find_kth(nums1, begin1, end1, nums2, mid2 + 1, end2, k - m // 2 - 1)
