f7fb533e60448041f70eb9d81b96dd240d90d6e4
David Ding <chengjie.ding@gmail.com>
2023-10-08 16:11:41 +0800

    update code. (#144)

--- leetcode/combination_sum.py
+++ leetcode/combination_sum.py
@@ -4,23 +4,26 @@ from typing import List
 
 class Solution:
     def combinationSum(self, candidates: List[int], target: int) -> List[List[int]]:
-        def dfs(candidates: List[int],
-                begin: int, size: int,
-                nums: List[int],
-                target: int) -> None:
+        def dfs(
+            candidates: List[int],
+            begin: int,
+            end: int,
+            nums: List[int],
+            target: int,
+        ) -> None:
             if target < 0:
                 return
             if target == 0:
-                combinations.append(nums)
+                retval.append(nums)
                 return
 
-            for i in range(begin, size):
+            for i in range(begin, end):
                 elem = candidates[i]
-                dfs(candidates, i, size, nums + [elem], target - elem)
+                dfs(candidates, i, end, nums + [elem], target - elem)
 
         size = len(candidates)
         if size == 0:
             return []
-        combinations = []
+        retval = []
         dfs(candidates, 0, size, [], target)
-        return combinations
+        return retval
